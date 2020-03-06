9752b17fdf26e63daa9aeed68ce9fe52989c3766
robert14k <rkbroncos14@gmail.com>
2020-03-06 12:48:53 -0600

    fixed git, got mapping working, on to the action part!

--- frontend/src/components/BlogPost.jsx
+++ frontend/src/components/BlogPost.jsx
@@ -1,17 +1,29 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Card, Button } from "antd";
 
 class BlogPost extends Component {
   render() {
     return (
-      <div>
+      <div style={{ backgroundColor: "rgb(236, 236, 236)", height: "100vh" }}>
         <h2>Welcome to my blog!</h2>
         <hr />
         <h3>Blog Posts</h3>
-
-        <h1>{this.props.postTitle}</h1>
-        <h2>{this.props.author}</h2>
-        <p>{this.props.postContent}</p>
+        {this.props.posts.map(post => (
+          <div style={{ alignContent: "center" }}>
+            <Card
+              title={post.postTitle}
+              style={{ margin: "auto", width: "90vw" }}
+            >
+              <p>{post.postContent}</p>
+              <h5>{post.author}</h5>
+              <Button style={{ margin: 5 }}>Edit</Button>
+              <Button danger style={{ margin: 5 }}>
+                Delete
+              </Button>
+            </Card>
+          </div>
+        ))}
       </div>
     );
   }
@@ -19,9 +31,10 @@ class BlogPost extends Component {
 
 const mapStateToProps = state => {
   return {
-    postContent: state.postContent,
-    author: state.author,
-    postTitle: state.postTitle
+    // postContent: state.postContent,
+    // author: state.author,
+    // postTitle: state.postTitle
+    posts: state.posts
   };
 };
 
